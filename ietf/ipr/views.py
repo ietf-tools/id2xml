e3a61535f13322b5aaa1996486dd33072eb35e94
Henrik Levkowetz <henrik@levkowetz.com>
2007-04-26 23:39:49 +0000

    Added first part of ipr_update_list functionality  - Legacy-Id: 31

--- ietf/ipr/views.py
+++ ietf/ipr/views.py
@@ -1,7 +1,18 @@
-from django.template import loader, Context
-from django.http import HttpResponse
+from models import IprDetail
+from django.shortcuts import render_to_response as render
 
 def default(request):
-    t = loader.get_template("ipr/ipr_disclosure.html")
-    return HttpResponse(t.render(Context()))
+    return render("ipr/ipr_disclosure.html", {})
+
+def update(request):
+    generic_disclosures = IprDetail.objects.filter(status__in=[1,3], generic__exact=1)
+    specific_disclosures = IprDetail.objects.filter(status__in=[1,3], generic__exact=0, third_party__exact=0)
+    thirdpty_disclosures = IprDetail.objects.filter(status__in=[1,3], generic__exact=0, third_party__exact=1)
+
+    return render('ipr/ipr_update_list.html',
+        {
+            'generic_disclosures' : generic_disclosures.order_by(* ['-submitted_date', ] ),
+            'specific_disclosures': specific_disclosures.order_by(* ['-submitted_date', ] ),
+            'thirdpty_disclosures': thirdpty_disclosures.order_by(* ['-submitted_date', ] ),
+        } )
 

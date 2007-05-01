234cc4fce9047d9cc34a5e414de4bbba36269896
Bill Fenner <fenner@fenron.net>
2007-05-01 21:20:25 +0000

    Updated redirects tool, with 3-level model.
    This allows handling scripts with varying arguments based
     on the command= argument.
    Also commit an initial_data fixture and a shell script to
     recreate the fixture in a (hopefully) svn-friendly format.
     - Legacy-Id: 60

--- ietf/redirects/views.py
+++ ietf/redirects/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect,Http404
 import re
 
-from ietf.redirects.models import Redirect
+from ietf.redirects.models import Redirect, Command
 
 def redirect(request, path="", script=""):
     if path:
@@ -11,12 +11,24 @@ def redirect(request, path="", script=""):
     except Redirect.DoesNotExist:
 	raise Http404
     url = redir.url + "/"
+    (rest, remove) = (redir.rest, redir.remove)
     try:
-	url += redir.rest % request.REQUEST
+	cmd = redir.commands.all().get(command=request.REQUEST['command'])
+	if cmd.url:
+	    rest = cmd.url + "/" + cmd.suffix.rest
+	else:
+	    rest = cmd.suffix.rest
+	remove = cmd.suffix.remove
+    except Command.DoesNotExist:
+	pass	# it's ok, there's no more-specific request.
+    except IndexError:
+	pass	# it's ok, request didn't have 'command'.
+    try:
+	url += rest % request.REQUEST
     except:
 	# rest had something in it that request didn't have, so just
 	# redirect to the root of the tool.
 	pass
-    if redir.remove:
-	url = re.sub(re.escape(redir.remove) + "/?$", "", url)
+    if remove:
+	url = re.sub(re.escape(remove) + "/?$", "", url)
     return HttpResponseRedirect(url)

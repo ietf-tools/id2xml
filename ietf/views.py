36cb3c323f0767a03973a31d95cb8a40ebab755a
Henrik Levkowetz <henrik@levkowetz.com>
2007-05-02 22:20:04 +0000

    Excluding the feeds app from the apps links on the front page.  - Legacy-Id: 73

--- ietf/views.py
+++ ietf/views.py
@@ -6,7 +6,7 @@ def apps(request):
     paths = []
     for pattern in urls.urlpatterns:
         path = pattern.regex.pattern.split("/")[0][1:]
-        if not re.search("[^a-z]", path) and not path in ["my", ]:
+        if not re.search("[^a-z]", path) and not path in ["my", "feeds"]:
             paths.append(path)
     apps = list(set(paths))
     apps.sort()

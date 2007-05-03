379a517492e51f47b741633f842d10ad6abcfba5
Bill Fenner <fenner@fenron.net>
2007-05-03 23:16:26 +0000

    Add docstrings.
    Add keep_spacing (add &nbsp;) and format_textarea (replaces the
     function of the same name in IETF_UTILS.pm).
    Rewrite text_to_html in terms of existing functions.
     - Legacy-Id: 87

--- ietf/idtracker/templatetags/ietf_filters.py
+++ ietf/idtracker/templatetags/ietf_filters.py
@@ -1,5 +1,6 @@
 from django import template
-from django.utils.html import escape, fix_ampersands
+from django.utils.html import escape, fix_ampersands, linebreaks
+from django.template.defaultfilters import linebreaksbr
 try:
     from email import utils as emailutils
 except ImportError:
@@ -10,10 +11,16 @@ register = template.Library()
 
 @register.filter(name='expand_comma')
 def expand_comma(value):
+    """
+    Adds a space after each comma, to allow word-wrapping of
+    long comma-separated lists."""
     return value.replace(",", ", ")
 
 @register.filter(name='parse_email_list')
 def parse_email_list(value):
+    """
+    Parse a list of comma-seperated email addresses into
+    a list of mailto: links."""
     addrs = re.split(", ?", value)
     ret = []
     for addr in addrs:
@@ -27,10 +34,15 @@ def parse_email_list(value):
 # but let's wait until we understand what that's for.
 @register.filter(name='make_one_per_line')
 def make_one_per_line(value):
+    """
+    Turn a comma-separated list into a carraige-return-seperated list."""
     return re.sub(", ?", "\n", value)
 
 @register.filter(name='link_if_url')
 def link_if_url(value):
+    """
+    If the argument looks like a url, return a link; otherwise, just
+    return the argument."""
     if (re.match('(https?|mailto):', value)):
 	return "<a href=\"%s\">%s</a>" % ( fix_ampersands(value), escape(value) )
     else:
@@ -48,6 +60,8 @@ def add_scheme(value):
 
 @register.filter(name='timesum')
 def timesum(value):
+    """
+    Sum the times in a list of dicts; used for sql query debugging info"""
     sum = 0.0
     for v in value:
         sum += float(v['time'])
@@ -55,4 +69,21 @@ def timesum(value):
 
 @register.filter(name='text_to_html')
 def text_to_html(value):
-    return "<br>\n".join(escape(value).split("\n"))
+    return keep_spacing(linebreaks(escape(value)))
+
+@register.filter(name='keep_spacing')
+def keep_spacing(value):
+    """
+    Replace any two spaces with one &nbsp; and one space so that
+    HTML output doesn't collapse them."""
+    return value.replace('  ', '&nbsp; ')
+
+@register.filter(name='format_textarea')
+def format_textarea(value):
+    """
+    Escapes HTML, except for <b>, </b>, <br>.
+
+    Adds <br> at the end like the builtin linebreaksbr.
+
+    Also calls keep_spacing."""
+    return keep_spacing(linebreaksbr(escape(value).replace('&lt;b&gt;','<b>').replace('&lt;/b&gt;','</b>').replace('&lt;br&gt;','<br>')))

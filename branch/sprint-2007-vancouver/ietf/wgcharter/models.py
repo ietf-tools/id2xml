ec57dd1446a3d179989432829c5618ed1e24c0ab
Cullen Jennings <fluffy@cisco.com>
2007-12-01 23:05:12 +0000

    update  - Legacy-Id: 979

--- branch/sprint-2007-vancouver/ietf/wgcharter/models.py
+++ branch/sprint-2007-vancouver/ietf/wgcharter/models.py
@@ -1,25 +1,28 @@
 from django.db import models
 
-from ietf.idtracker import models
+from ietf.idtracker.models import PersonOrOrgInfo
 
 # Create your models here.
 
 
+class WGCharterInfo(models.Model):
+    id = models.AutoField(primary_key=True)
+    #approvedVersion = models.ForeignKey(CharterVersion,null=True,editable=False)
+    #recentDraftVersion = models.ForeignKey(CharterVersion,null=True,editable=False)
+
 class CharterVersion(models.Model):
+    version_id = models.AutoField(primary_key=True)
     STATE_CHOICES = (
 	('draft','Draft'),
 	('lastCalled','Sent to IETF Last Call'),
 	('approved','Approved'),
 	)
-     version_id = models.AutoField(primary_key=True)
-     creation_date = models.DateField(null=True, blank=True)
-     state = models.CharField(maxlength=30,choices=STATE_CHOICES,blank=False,default='draft')
-     text = models.TextField(blank=True)
-     submitter = models.ForeignKey(PersonOrOrgInfo,null=False,editable=False)
+    creation_date = models.DateField(null=True, blank=True)
+    state = models.CharField(maxlength=30,choices=STATE_CHOICES,blank=False,default='draft')
+    wg_charter_info = models.ForeignKey(WGCharterInfo,null=True,editable=False)
+    text = models.TextField(blank=True)
+    submitter = models.ForeignKey(PersonOrOrgInfo,null=False,editable=False)
+    
 
-class WGCharterInfo:
-    id = models.AutoField(primary_key=True)
-    approvedVersion = models.ForeignKey(CharterVersion,null=True,editable=False)
-    recentDraftVersion = models.ForeignKey(CharterVersion,null=True,editable=False)
-
+    
 

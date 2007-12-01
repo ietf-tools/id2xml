1d5b05bd499443a2a2dc6e01152d3c95842461ff
Cullen Jennings <fluffy@cisco.com>
2007-12-01 22:48:38 +0000

    updated model  - Legacy-Id: 978

--- branch/sprint-2007-vancouver/ietf/wgcharter/models.py
+++ branch/sprint-2007-vancouver/ietf/wgcharter/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+from ietf.idtracker import models
+
 # Create your models here.
+
+
+class CharterVersion(models.Model):
+    STATE_CHOICES = (
+	('draft','Draft'),
+	('lastCalled','Sent to IETF Last Call'),
+	('approved','Approved'),
+	)
+     version_id = models.AutoField(primary_key=True)
+     creation_date = models.DateField(null=True, blank=True)
+     state = models.CharField(maxlength=30,choices=STATE_CHOICES,blank=False,default='draft')
+     text = models.TextField(blank=True)
+     submitter = models.ForeignKey(PersonOrOrgInfo,null=False,editable=False)
+
+class WGCharterInfo:
+    id = models.AutoField(primary_key=True)
+    approvedVersion = models.ForeignKey(CharterVersion,null=True,editable=False)
+    recentDraftVersion = models.ForeignKey(CharterVersion,null=True,editable=False)
+
+

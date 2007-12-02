8f37f5ef2513bfdfb09ca4b244a4db8731dee0ce
Eric Rescorla <ekr@rtfm.com>
2007-12-02 00:37:12 +0000

    URLs  - Legacy-Id: 989

---
+++ branch/sprint-2007-vancouver/ietf/wgcharter/urls.py
@@ -0,0 +1,11 @@
+from ietf.wgcharter import views
+from django.conf.urls.defaults import patterns
+
+urlpatterns = patterns('',
+        (r'^(?P<wgname>[a-z]+)/$',views.current),
+        (r'^(?P<wgname>[a-z]+)/all/$',views.list),
+        (r'^(?P<wgname>[a-z]+)/(?P<version>\d+)/status/$',views.draft_status),
+        (r'^(?P<wgname>[a-z]+)/(?P<version1>\d+|[a-z]+)/(?P<version2>\d+|[a-z]+)/$',views.diff),
+        (r'^(?P<wgname>[a-z]+)/(?P<version>\d+|[a-z]+)/$',views.draft),
+)
+
--- branch/sprint-2007-vancouver/ietf/wgcharter/views.py
+++ branch/sprint-2007-vancouver/ietf/wgcharter/views.py
@@ -1,1 +1,23 @@
 # Create your views here.
+from django.http import HttpResponse
+import datetime
+
+def current(request, wgname):
+    html = "<html><body>Current Draft View, WG=%s</body></html>" % wgname
+    return HttpResponse(html)
+
+def list(request, wgname):
+    html = "<html><body>List Drafts View, WG=%s</body></html>"  % wgname
+    return HttpResponse(html)
+
+def diff(request, wgname, version1, version2):
+    html = "<html><body>Diff Drafts View, WG=%s, first=%s, second=%s</body></html>" % (wgname, version1, version2)
+    return HttpResponse(html)
+
+def draft(request, wgname, version):
+    html = "<html><body>Draft Drafts View, WG=%s, version=%s</body></html>" % (wgname, version)
+    return HttpResponse(html)
+
+def draft_status(request, wgname, version):
+    html = "<html><body>Status Drafts View, WG=%s, version=%s</body></html>" % (wgname, version)
+    return HttpResponse(html)

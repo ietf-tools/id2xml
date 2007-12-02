8c4d0259c7b899e8bf917bcce05db89185a06c40
Cullen Jennings <fluffy@cisco.com>
2007-12-02 01:08:16 +0000

    updated  - Legacy-Id: 994

--- branch/sprint-2007-vancouver/ietf/wgcharter/urls.py
+++ branch/sprint-2007-vancouver/ietf/wgcharter/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.defaults import patterns
 urlpatterns = patterns('',
         (r'^(?P<wgname>[a-z]+)/$',views.current),
         (r'^(?P<wgname>[a-z]+)/all/$',views.list),
+        (r'^(?P<wgname>[a-z]+)/add/$',views.add),
         (r'^(?P<wgname>[a-z]+)/(?P<version>\d+)/status/$',views.draft_status),
         (r'^(?P<wgname>[a-z]+)/(?P<version1>\d+|[a-z]+)/(?P<version2>\d+|[a-z]+)/$',views.diff),
         (r'^(?P<wgname>[a-z]+)/(?P<version>\d+|[a-z]+)/$',views.draft),
--- branch/sprint-2007-vancouver/ietf/wgcharter/views.py
+++ branch/sprint-2007-vancouver/ietf/wgcharter/views.py
@@ -7,14 +7,20 @@ from django import oldforms as forms
 from django import newforms as forms
 from django.newforms import form_for_model,form_for_instance
 
+from ietf.wgcharter.models import  CharterVersion
+
 
 def current(request, wgname):
     html = "<html><body>Current Draft View, WG=%s</body></html>" % wgname
     return HttpResponse(html)
 
+def add(request, wgname):
+    html = "<html><body>Add  View, WG=%s</body></html>" % wgname
+    return HttpResponse(html)
+
 def list(request, wgname):
     charters = CharterVersion.objects.all()
-    return render_to_response('all.html', {'wgname':wgname,'charterList': charters})
+    return render_to_response('wgcharter/all.html', {'wgname':wgname,'charterList': charters})
 
 def diff(request, wgname, version1, version2):
     html = "<html><body>Diff Drafts View, WG=%s, first=%s, second=%s</body></html>" % (wgname, version1, version2)

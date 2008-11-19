c61fc77a811eccc64eeae3780f8627db945ac87d
Jelte Jansen <github@tjeb.nl>
2008-11-19 17:55:38 +0000

    start with html output (so abstracts.py can replace both update-abstracts and update-abstracts-html, and optionally both at once, so the data does not have to be pulled from the db twice at each run)  - Legacy-Id: 1271

--- jelte/ietf/bin/abstracts.py
+++ jelte/ietf/bin/abstracts.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from django.conf import settings
+from django.template.loader import render_to_string
 from ietf.idtracker.models import InternetDraft, Area, Acronym, AreaGroup, IETFWG, IDAuthor
 import sys
 
@@ -16,10 +17,12 @@ def get_intro(id_index=False):
     sites directory.  These drafts are listed alphabetically by working
     group acronym and start date."""
 
-def group_text(group):
+def group_string(group):
   text =  group.group_acronym.name + " (" + group.group_acronym.acronym + ")"
-  lines = "-" * len(text)
-  return text + "\n" + lines + "\n"
+  return text
+
+def dashes_for_string(string):
+  return len(string) * "-"
 
 def draft_authors(draft):
   authors = IDAuthor.objects.filter(document=draft).order_by('author_order')
@@ -55,46 +58,83 @@ def wrap_and_indent(text, width=74, indent=0):
     result.append(indent*" " + " ".join(cur_line_words))
   return "\n".join(result)
 
-def print_abstracts_text(acronym, no_abstracts):
+# if txt_file is not None, .txt output will be writte to
+# that file
+# if html_file is not None (eg. 1id_abstracts.html), an overview
+# will be written to this file
+# if html_directory is not None, html files per group will
+# be created in this directory, and an overview will be
+def print_abstracts_text(acronym, no_abstracts, txt_file, html_file, html_directory, silent=False):
   # if you want to store everythinh in a string instead of printing,
   # remember not to use str + str, but make a list for it and use join()
-  if acronym:    
+  if acronym:
     groups = IETFWG.objects.filter(areagroup__area__area_acronym__acronym=acronym).order_by('group_acronym')
   else:
     groups = IETFWG.objects.all();
 
-  print get_intro(no_abstracts)
-  print ""
-  print ""
+  html_group_elements = []
+
+  if txt_file:
+    txt_file.write(get_intro(no_abstracts))
+    txt_file.write("\n")
+    txt_file.write("\n")
+    txt_file.write("\n")
+
   for group in groups:
+    if not silent:
+      print group.group_acronym.acronym
+
     drafts = group.active_drafts()
+
     if len(drafts) > 0:
-      print group_text(group)
+      group_text = group_string(group)
+      
+      if txt_file:
+        txt_file.write(group_text)
+        txt_file.write("\n")
+        txt_file.write(dashes_for_string(group_text))
+        txt_file.write("\n")
+        txt_file.write("\n")
+      if html_file:
+        html_group_elements.append({'name': group_text,
+             'rel_url': html_directory + "/" + group.group_acronym.acronym,
+             'active_draft_count': len(drafts)
+             })
       for draft in drafts:
-        title_parts = []
-        title_parts.append(draft_title_text(draft))
-        title_parts.append(draft_authors(draft))
-        title_parts.append(str(draft.revision_date))
-        title_parts.append("<" + draft.filename + draft.file_type + ">")
-        print wrap_and_indent(", ".join(title_parts), 80, 2)
-        #print len(", ".join(title_parts))
-        print ""
-        if not no_abstracts:
-          print wrap_and_indent(draft_abstract_text(draft), 80, 4)
-          print ""
-        
+        if txt_file:
+          title_parts = []
+          title_parts.append(draft_title_text(draft))
+          title_parts.append(draft_authors(draft))
+          title_parts.append(str(draft.revision_date))
+          title_parts.append("<" + draft.filename + draft.file_type + ">")
+          txt_file.write(wrap_and_indent(", ".join(title_parts), 80, 2))
+          txt_file.write("\n")
+          txt_file.write("\n")
+          if not no_abstracts:
+            txt_file.write(wrap_and_indent(draft_abstract_text(draft), 80, 4))
+            txt_file.write("\n")
+            txt_file.write("\n")
+
+  if html_file:
+    html_context = {}
+    html_context['groups'] = html_group_elements
+    html_file.write(render_to_string("idtracker/idtracker_abstracts.html", html_context))
+
 
 def usage():
   print "Usage: abstracts [OPTIONS]"
   print ""
   print "Options:"
-  print "-noabstracts\t\tDo not print abstracts"
+  print "-idindex\t\tDo not print abstracts"
   print "-h or --help:\t\tPrint this text"
   print "-area <area acronym>\tOnly print abstracts for specific area"
 
 # when called from command line
 if __name__ == "__main__":
   no_abstracts = False
+  txt_file = None
+  html_file = None
+  html_directory = "id.by.wg"
   if len(sys.argv) == 1:
     usage()
     sys.exit(1)
@@ -102,10 +142,19 @@ if __name__ == "__main__":
     if arg == "-h" or arg == "--help":
       usage()
       sys.exit(0)
-    if arg == "-noabstracts":
+    if arg == "-idindex":
       no_abstracts = True
     elif arg == "-all":
       area_acronym = None
+    elif arg == "-txt-output":
+      txt_file = open("/tmp/abstr.txt", "w")
+    elif arg == "-html-output":
+      html_file = open("/tmp/abstr.html", "w")
     else:
       area_acronym = arg
-  print_abstracts_text(area_acronym, no_abstracts)
+  print_abstracts_text(area_acronym, no_abstracts, txt_file, html_file, html_directory, False)
+  if (txt_file):
+    txt_file.close()
+  if html_file:
+    html_file.close()
+

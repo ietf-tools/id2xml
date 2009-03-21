de3b6a9439e0370ab1ba114e5b42663a388d756f
Bill Fenner <fenner@fenron.net>
2009-03-21 17:22:54 +0000

    Add the RFC Editor hash, to get the rolodex admin page working again.  - Legacy-Id: 1377

--- test/sql_fixup.sql
+++ test/sql_fixup.sql
@@ -1,5 +1,6 @@
 -- This file holds needed corrections to the database until they have been applied to
 -- the live database.  This file is applied after importing a new dump of the live DB.
 
+ALTER TABLE ietfauth_usermap ADD rfced_htdigest VARCHAR(32);
 
 

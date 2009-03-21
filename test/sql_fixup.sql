a444de7ce36e4d2a9ade815966f60ee8ea707514
Bill Fenner <fenner@fenron.net>
2009-03-21 17:29:54 +0000

    Oops, the migration from the current db dumps needs the email field too.  - Legacy-Id: 1378

--- test/sql_fixup.sql
+++ test/sql_fixup.sql
@@ -1,6 +1,7 @@
 -- This file holds needed corrections to the database until they have been applied to
 -- the live database.  This file is applied after importing a new dump of the live DB.
 
+ALTER TABLE ietfauth_usermap ADD email_htdigest VARCHAR(32);
 ALTER TABLE ietfauth_usermap ADD rfced_htdigest VARCHAR(32);
 
 

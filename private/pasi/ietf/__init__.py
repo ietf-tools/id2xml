25f9099cb5ad539b1324e794abaea4a33de06de7
Pasi Eronen <pe@iki.fi>
2009-07-25 07:37:39 +0000

    Preparing for a fresh branch  - Legacy-Id: 1515

--- private/pasi/ietf/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright The IETF Trust 2007, All Rights Reserved
-
-__version__ = "2.27-dev"
-
-__date__    = "$Date$"
-
-__rev__     = "$Rev$ (dev) Latest release: Rev. 1481 "
-
-__id__      = "$Id$"

adb6495a0f428f1f121f1eb8d5b5cc97aa72d0f4
Henrik Levkowetz <henrik@levkowetz.com>
2009-07-25 08:26:03 +0000

    IETF 75 Sprint branch for Pasi Eronen  - Legacy-Id: 1523

---
+++ pasi/ietf/idindex/urls.py
@@ -0,0 +1,34 @@
+# Copyright The IETF Trust 2007, All Rights Reserved
+
+from django.conf.urls.defaults import patterns
+from ietf.idtracker.models import InternetDraft
+from ietf.idindex import views
+from ietf.idindex.views import alphabet, orgs
+
+info_dict = {
+    'queryset': InternetDraft.objects.all(),
+    'template_name': 'idindex/internetdraft_detail.html',
+    'extra_context': {
+	'alphabet': alphabet,
+	'orgs': orgs,
+    }
+}
+
+urlpatterns = patterns('',
+     (r'^wgid/(?P<id>\d+)/$', views.wgdocs_redir),
+     (r'^wg/(?P<wg>[^/]+)/$', views.wgdocs),
+     (r'^ind/(?P<filter>[^/]+)/$', views.inddocs),
+     (r'^other/(?P<cat>[^/]+)/$', views.otherdocs),
+     # (?P<cat>(?:all|rfc|current|dead)) really confuses reverse()
+     (r'^(?P<cat>all)/$', views.showdocs),
+     (r'^(?P<cat>rfc)/$', views.showdocs),
+     (r'^(?P<cat>current)/$', views.showdocs),
+     (r'^(?P<cat>dead)/$', views.showdocs),
+     (r'^(?P<id>\d+)/related/$', views.redirect_related),
+     (r'^(?P<slug>[^/]+)/related/$', views.view_related_docs),
+     (r'^(?P<object_id>\d+)/$', views.redirect_id),
+     (r'^(?P<slug>[^/]+)/$', views.view_id, dict(info_dict, slug_field='filename')),
+     (r'^all_id_txt.html$', views.all_id, { 'template_name': 'idindex/all_id_txt.html' }),
+     (r'^all_id.html$', views.all_id, { 'template_name': 'idindex/all_id.html' }),
+     (r'^$', views.search),
+)

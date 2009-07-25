64dfb7b515f87f5fa98eabd785136bb0ada4c928
Adam Roach <adam@nostrum.com>
2009-07-25 13:11:25 +0000

    Adding WG descriptions directory  - Legacy-Id: 1556

---
+++ adam/wg-descriptions/samba.bat
@@ -0,0 +1,9 @@
+
+net use M: /delete
+net use M: \\10.27.2.28\id (ata9ult /USER:cfusion /persistent:yes
+
+net use N: /delete
+net use N: \\10.27.2.28\charters (ata9ult /USER:cfusion /persistent:yes
+
+net use X: /delete
+net use X: \\10.27.2.28\ietfhome (ata9ult /USER:cfusion /persistent:yes

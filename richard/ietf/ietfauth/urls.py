f20a0a2fa77e753bdcb556fc5affb7f540337a30
Henrik Levkowetz <henrik@levkowetz.com>
2010-03-20 16:43:03 +0000

    IETF 77 Sprint branch for Richard Barnes  - Legacy-Id: 2094

---
+++ richard/ietf/ietfauth/urls.py
@@ -0,0 +1,10 @@
+# Copyright The IETF Trust 2007, 2009, All Rights Reserved
+
+from django.conf.urls.defaults import patterns
+from ietf.ietfauth import views
+
+urlpatterns = patterns('',
+        (r'^login/$', views.ietf_login),
+        (r'^loggedin/$', views.ietf_loggedin),
+	(r'^profile/$', views.profile)
+)

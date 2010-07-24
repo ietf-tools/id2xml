0201f457df636134bf6aff2719219aad87999958
Adam Roach <adam@nostrum.com>
2010-07-24 15:21:31 +0000

    Added fragment selection of WGs, areas. Now, we need to add an interface to manipulate it. :)  - Legacy-Id: 2408

--- adam/ietf/meeting/views.py
+++ adam/ietf/meeting/views.py
@@ -4,6 +4,7 @@
 #import models
 from django.shortcuts import render_to_response, get_object_or_404
 from ietf.proceedings.models import Meeting, MeetingTime, WgMeetingSession, MeetingVenue, IESGHistory, Proceeding, Switches, WgProceedingsActivities
+from ietf.idtracker.models import IETFWG, IRTF
 from django.views.generic.list_detail import object_list
 from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.core.urlresolvers import reverse
@@ -91,6 +92,9 @@ def agenda_info(num=None):
 @decorator_from_middleware(GZipMiddleware)
 def html_agenda(request, num=None):
     timeslots, update, meeting, venue, ads, plenaryw_agenda, plenaryt_agenda = agenda_info(num)
+    wgs = IETFWG.objects.filter(status=IETFWG.ACTIVE)
+    rgs = IRTF.objects.all()
+
     if  settings.SERVER_MODE != 'production' and '_testiphone' in request.REQUEST:
         user_agent = "iPhone"
     elif 'user_agent' in request.REQUEST:
@@ -106,7 +110,8 @@ def html_agenda(request, num=None):
         template = "meeting/agenda.html"
     return render_to_response(template,
             {"timeslots":timeslots, "update":update, "meeting":meeting, "venue":venue, "ads":ads,
-                "plenaryw_agenda":plenaryw_agenda, "plenaryt_agenda":plenaryt_agenda, },
+                "plenaryw_agenda":plenaryw_agenda, "plenaryt_agenda":plenaryt_agenda, 
+                "wg_list" : wgs, "rg_list" : rgs},
             context_instance=RequestContext(request))
 
 def text_agenda(request, num=None):

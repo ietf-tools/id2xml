e6ff3d179585208d232a6cc806a67905b7f2b1ee
Henrik Levkowetz <henrik@levkowetz.com>
2010-08-12 14:58:58 +0000

    Remove this again -- we'll make it a one level deeper hierarchy in a moment...  - Legacy-Id: 2471

--- private/rjs/ietf/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright The IETF Trust 2007, All Rights Reserved
-
-__version__ = "3.05-dev"
-
-__date__    = "$Date$"
-
-__rev__     = "$Rev$ (dev) Latest release: Rev. 2451 "
-
-__id__      = "$Id$"

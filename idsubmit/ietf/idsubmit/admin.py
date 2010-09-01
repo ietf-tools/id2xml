72ff0da03e40c93045d09ee23395d4927ee7af01
Wiggins <wiggins@concentricsky.com>
2010-09-01 20:41:55 +0000

    define admin for idsubmit models  - Legacy-Id: 2514

--- idsubmit/ietf/idsubmit/admin.py
+++ idsubmit/ietf/idsubmit/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from ietf.idsubmit.models import *
 
+class SubmissionEnvAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(SubmissionEnv, SubmissionEnvAdmin)
+
 class IdSubmissionDetailAdmin(admin.ModelAdmin):
     raw_id_fields = ('group','submitter')
     save_on_top = True
-    search_fields = ('filename','title')
-    list_display = ('__unicode__','group','submission_date','status_id')
+    search_fields = ('filename','title','group__name','submitter__first_name','submitter__last_name')
+    list_display = ('__unicode__','group','submitter','submission_date','status_id')
     list_filter = ('submission_date','status_id','group')
     fieldsets = (
         (None, {'fields': ('title','filename','revision','file_type','filesize','invalid_version')}),
@@ -18,3 +22,9 @@ class IdSubmissionDetailAdmin(admin.ModelAdmin):
 
     )
 admin.site.register(IdSubmissionDetail, IdSubmissionDetailAdmin)
+
+class IdApprovedDetailAdmin(admin.ModelAdmin):
+    raw_id_fields = ('approved_person','recorded_by')
+    list_display = ('filename','approved_status','approved_person','approved_date','recorded_by')
+    search_fields = ('filename', 'approved_person__first_name','approved_person__last_name', 'recorded_by__first_name','recorded_by__last_name')
+admin.site.register(IdApprovedDetail, IdApprovedDetailAdmin)

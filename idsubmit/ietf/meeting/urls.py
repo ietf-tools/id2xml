f6df7fd1cf15b400ba20f08dd34ddde2b5c00846
Wiggins <wiggins@concentricsky.com>
2010-10-15 22:21:50 +0000

    comment out missing meeting views to quash errors  - Legacy-Id: 2558

--- idsubmit/ietf/meeting/urls.py
+++ idsubmit/ietf/meeting/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls.defaults import patterns
 from ietf.meeting import views
 
 urlpatterns = patterns('',
-    (r'^request/(?P<group_id>\d+)/$', views.schedule_group),
-    (r'^request/$', views.show_schedule),
+    #(r'^request/(?P<group_id>\d+)/$', views.schedule_group),
+    #(r'^request/$', views.show_schedule),
     (r'^(?P<meeting_num>\d+)/materials.html$', views.show_html_materials),
     (r'^agenda/$', views.html_agenda),
     (r'^agenda.html$', views.html_agenda),

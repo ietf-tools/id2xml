9055024e0fff18705644e817a403e9aaefbd3cfc
Henrik Levkowetz <henrik@levkowetz.com>
2010-11-10 07:08:36 +0000

    New IETF 79 coding branch for Henrik Levkowetz  - Legacy-Id: 2662

---
+++ henrik/ietf/meeting/urls.py
@@ -0,0 +1,18 @@
+# Copyright The IETF Trust 2007, All Rights Reserved
+
+from django.conf.urls.defaults import patterns
+from ietf.meeting import views
+
+urlpatterns = patterns('',
+    (r'^(?P<meeting_num>\d+)/materials.html$', views.show_html_materials),
+    (r'^agenda/$', views.html_agenda),
+    (r'^agenda(?:.html)?$', views.html_agenda),
+    (r'^agenda.txt$', views.text_agenda),
+    (r'^(?P<num>\d+)/agenda(?:.html)?/?$', views.html_agenda),
+    (r'^(?P<num>\d+)/agenda.txt$', views.text_agenda),
+    (r'^(?P<num>\d+)/agenda/(?P<session>[A-Za-z0-9-]+)-drafts.pdf$', views.session_draft_pdf),
+    (r'^(?P<num>\d+)/agenda/(?P<session>[A-Za-z0-9-]+)-drafts.tgz$', views.session_draft_tarfile),
+    (r'^(?P<num>\d+)/agenda/(?P<session>[A-Za-z0-9-]+)(?P<ext>\.[A-Za-z0-9]+)?$', views.session_agenda),
+    (r'^$', views.current_materials),
+)
+

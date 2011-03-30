3a88e0c5e614336bb04278c5294968ccf63da68c
Henrik Levkowetz <henrik@levkowetz.com>
2011-03-30 14:27:35 +0000

    The brances under 'private' aren't really private, but they are personal.
    [[Split portion of a mixed commit.]]
     - Legacy-Id: 3005.9

--- private/rjs/ballotDialog/ietf/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright The IETF Trust 2007, All Rights Reserved
-
-__version__ = "3.06-dev"
-
-__date__    = "$Date$"
-
-__rev__     = "$Rev$ (dev) Latest release: Rev. 2505 "
-
-__id__      = "$Id$"

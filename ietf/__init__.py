a8566202a923f9d96d088f8745a9881b891d7579
Adam Roach <adam@nostrum.com>
2011-06-27 21:45:37 +0000

    Trying something out for Marc Petit-Hugenin
    [[Split portion of a mixed commit.]]
     - Legacy-Id: 3183.9

--- ietf/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright The IETF Trust 2007, All Rights Reserved
-
-__version__ = "3.02-dev"
-
-__date__    = "$Date$"
-
-__rev__     = "$Rev$ (dev) Latest release: Rev. 2358 "
-
-__id__      = "$Id$"

599819e309ed476de51c0b1594bd9ec2d31927ed
Henrik Levkowetz <henrik@levkowetz.com>
2011-10-23 16:06:52 +0000

    [[Split portion of a mixed commit.]]  - Legacy-Id: 3460.6

--- __init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright The IETF Trust 2007, All Rights Reserved
-
-__version__ = "3.56-dev"
-
-__date__    = "$Date$"
-
-__rev__     = "$Rev$ (dev) Latest release: Rev. 3148 "
-
-__id__      = "$Id$"

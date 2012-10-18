e2e4dbf75422118ef6ffe4277bdc7d8769cbf9f5
Henrik Levkowetz <henrik@levkowetz.com>
2012-10-18 13:36:04 +0000

    Move old stuff out of the way.
    [[Split portion of a mixed commit.]]
     - Legacy-Id: 4927.1

--- branch/iesg_idtracker/test/sql_fixup.sql
+++
@@ -1,6 +0,0 @@
--- This file holds needed corrections to the database until they have been applied to
--- the live database.  This file is applied after importing a new dump of the live DB.
-
--- Add primary key to telchat_dates table
-alter table telechat_dates add id integer primary key auto_increment;
-

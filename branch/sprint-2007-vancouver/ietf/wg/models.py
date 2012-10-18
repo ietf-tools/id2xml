2df1805338cc6e703367a39487988a113922f008
Henrik Levkowetz <henrik@levkowetz.com>
2012-10-18 13:35:19 +0000

    Move old stuff out of the way.
    [[Split portion of a mixed commit.]]
     - Legacy-Id: 4924.1

--- branch/sprint-2007-vancouver/ietf/wg/models.py
+++
@@ -1,4 +0,0 @@
-# Copyright The IETF Trust 2007, All Rights Reserved
-
-from ietf.idtracker.models import Area, PersonOrOrgInfo, AreaDirector, WGChair, WGSecretary, IETFWG
-
--- branch/sprint-scratchpad/ietf/iesg/sitemaps.py
+++
@@ -1,4 +0,0 @@
-from django.contrib.sitemaps import GenericSitemap
-from ietf.iesg.urls import telechat_detail
-
-IESGMinutesMap = GenericSitemap(telechat_detail, changefreq="never")

1057dbf631c22669cf35d0d52769945b9836f516
Joel M. Halpern <jmh@joelhalpern.com>
2012-11-03 19:36:05 +0000

    Make comments optional on WG adoption and WG state changes.  - Legacy-Id: 5001

--- debug.py
+++ debug.py
@@ -1,8 +1,15 @@
 import sys
 import time as timeutils
 import inspect
-import syslog
-from pprint import pformat
+try:
+    import syslog
+    write = syslog.syslog
+except ImportError:                     # import syslog will fail on Windows boxes
+    import sys
+    write = lambda x: sys.stderr.write(x+"\n")
+except ImportError:
+    from pprint import pformat
+    
 import cProfile
 try:
     from django.conf import settings

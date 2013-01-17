b582f406bb5926d63efaf4ed8340fdb74eab344b
Michael Richardson <mcr@sandelman.ca>
2013-01-17 04:12:02 +0000

    some test cases for groups (#899)  - Legacy-Id: 5231

--- v4.37.2/ietf/group/tests/workinggroups.py
+++ v4.37.2/ietf/group/tests/workinggroups.py
@@ -5,10 +5,13 @@ from ietf.group.models import Group
 class WorkingGroupTestCase(TestCase):
     fixtures = [ 'workinggroups.json']
 
-    def FindOneWg(self):
+    def test_FindOneWg(self):
         one = Group.objects.filter(acronym = 'roll')
         self.assertIsNotNone(one)
         
-    def ActiveWgGroupList(self):
-        True
+    def test_ActiveWgGroupList(self):
+        groups = Group.objects.active_wgs()
+        self.assertEqual(groups.count(), 151)
+
+
         

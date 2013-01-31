115632bab165ea59707c7632f78524dc1dcf7873
Henrik Levkowetz <henrik@levkowetz.com>
2013-01-31 11:02:40 +0000

    Remove badly named personal repository 'ietf'.  - Legacy-Id: 5349

--- ietf/person/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns('',
-        (r'^search/$', "ietf.person.views.ajax_search_emails", None, 'ajax_search_emails'),
-)
--- static/js/history.js
+++
@@ -1,5 +0,0 @@
-jQuery(function () {
-    jQuery("table.history .snippet .showAll").click(function () {
-        jQuery(this).parents(".snippet").hide().siblings(".full").show();
-    });
-});

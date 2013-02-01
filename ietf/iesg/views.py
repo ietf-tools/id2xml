e7edec2e22d5b81b46d7631c5451741461f8f01e
Russ Housley <housley@vigilsec.com>
2013-02-01 20:45:43 +0000

    Correct handling of RFC documents in iesg/agenda/agenda.json  - Legacy-Id: 5351

--- ietf/iesg/views.py
+++ ietf/iesg/views.py
@@ -319,44 +319,41 @@ def _agenda_json(request, date=None):
                         docinfo = {'docname':d.canonical_name(),
                                    'title':d.title,
                                    'ad':d.ad.name}
+                        if d.rfc_number():
+                            docinfo['rfc-number'] = d.rfc_number()
+                        else:
+                            docinfo['rev'] = d.rev
                         if d.note:
                             docinfo['note'] = d.note
                         defer = d.active_defer_event()
                         if defer:
                             docinfo['defer-by'] = defer.by.name
                             docinfo['defer-at'] = str(defer.time)
-			if doc.type_id == "draft":
-                            docinfo['intended-std-level'] = str(doc.intended_std_level)
-                            if doc.rfc_number():
-                                docinfo['rfc-number'] = doc.rfc_number()
-                            else:
-                                docinfo['rev'] = doc.rev
-
-                            iana_state = doc.get_state("draft-iana-review")
-                            if iana_state.slug in ("not-ok", "changed", "need-rev"):
-                                docinfo['iana_review_state'] = str(iana_state)
-
-                            if doc.get_state_slug("draft-iesg") == "lc":
-                                e = doc.latest_event(LastCallDocEvent, type="sent_last_call")
+                        if d.type_id == "draft":
+                            docinfo['intended-std-level'] = str(d.intended_std_level)
+                            iana_state = d.get_state("draft-iana-review")
+                            if iana_state:
+                                if iana_state.slug in ("not-ok", "changed", "need-rev"):
+                                    docinfo['iana-review-state'] = str(iana_state)
+                            if d.get_state_slug("draft-iesg") == "lc":
+                                e = d.latest_event(LastCallDocEvent, type="sent_last_call")
                                 if e:
-                                    docinfo['lastcall_expires'] = e.expires
-
+                                    docinfo['lastcall-expires'] = e.expires
                             docinfo['consensus'] = None
-                            e = doc.latest_event(ConsensusDocEvent, type="changed_consensus")
+                            e = d.latest_event(ConsensusDocEvent, type="changed_consensus")
                             if e:
                                 docinfo['consensus'] = e.consensus
-                        elif doc.type_id == 'conflrev':
-                            docinfo['rev'] = doc.rev
-                            td = doc.relateddocument_set.get(relationship__slug='conflrev').target.document
+                        elif d.type_id == 'conflrev':
+                            td = d.relateddocument_set.get(relationship__slug='conflrev').target.document
                             docinfo['target-docname'] = td.canonical_name()
                             docinfo['target-title'] = td.title
                             docinfo['target-rev'] = td.rev
                             docinfo['intended-std-level'] = str(td.intended_std_level)
                             docinfo['stream'] = str(td.stream)
-			else:
-			    # XXX check this -- is there nothing to set for
-			    # all other documents here?
-			    pass
+                        else:
+                            # XXX check this -- is there nothing to set for
+                            # all other documents here?
+                            pass
                         data['sections'][s]['docs'] += [docinfo, ]
 
     wgs = agenda_wg_actions(date)

cbc96a26eaea09cf654566c815529a20faf73866
Michael Richardson <mcr@sandelman.ca>
2013-02-03 20:46:02 +0000

    test case for agenda_info, along with enough fixtures to make it work (#899)  - Legacy-Id: 5377

--- v4.41/ietf/meeting/tests/agenda.py
+++ v4.41/ietf/meeting/tests/agenda.py
@@ -3,19 +3,33 @@ from django.test import TestCase
 from django.test.client import Client
 
 class AgendaInfoTestCase(TestCase):
-    fixtures = [ 'meeting85.json', 'workinggroups.json', 'person.json', 'users.json' ]
+    fixtures = [ 'names.xml',  # for MeetingTypeName
+                 'meeting83.json',
+                 'workinggroups.json',
+                 'person.json', 'users.json' ]
 
     def test_AgendaInfo(self):
         from ietf.meeting.views import agenda_info
-        num = 85
+        num = '83'
         timeslots, update, meeting, venue, ads, plenaryw_agenda, plenaryt_agenda = agenda_info(num)
-        self.assertEqual(len(timeslots),99)
-        self.assertEqual(update, "foo")
-        self.assertEqual(meeting.number, 85)
-        self.assertEqual(venue, "Atlanta")
-        self.assertEqual(len(ads), 11)
-        self.assertEqual(plenaryw_agenda, "IAB")
-        self.assertEqual(plenaryt_agenda, "IESG")
+        # I think that "timeslots" here, is unique times, not actually
+        # the timeslots array itself.
+        self.assertEqual(len(timeslots),26)
+        self.assertEqual(meeting.number,'83')
+        self.assertEqual(venue.meeting_num, "83")
+        self.assertEqual(len(ads), 0)
+
+    def test_AgendaInfoNotFound(self):
+        from django.http import Http404
+        from ietf.meeting.views import agenda_info
+        num = '83b'
+        # should raise an exception.
+        try:
+            timeslots, update, meeting, venue, ads, plenaryw_agenda, plenaryt_agenda = agenda_info(num)
+            False
+        except Http404:
+            pass
+
 
         
         

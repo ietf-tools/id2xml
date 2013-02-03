61cf1270d4981830ebbc625e0c75246098be609a
Michael Richardson <mcr@sandelman.ca>
2013-02-03 20:52:19 +0000

    info side panel  - Legacy-Id: 5414

--- v4.41/static/js/agenda_edit.js
+++ v4.41/static/js/agenda_edit.js
@@ -26,15 +26,17 @@ function log(text){
 */
 function initStuff(){
     log("initstuff() ran");
-    listeners();
-    log("listeners() ran");
     setup_slots();
     log("setup_slots() ran");
     droppable();
     log("droppable() ran");
     load_events();
     log("load_events() ran");
+
+    listeners();
+    log("listeners() ran");
     hide_empty();
+
 }
 
 /* hide_empty()
@@ -83,35 +85,68 @@ function load_events(){
 
 /* Dump listeners into here */
 function listeners(){
-    $('#ToggleCalendar').click(function(){
-	$('#calendar').toggle();
-    });
-    
-    // button to add event
-    $('#AddEvent').click(function(){
-	add_event();
-    });
+    $('.meeting_event').unbind('click'); // If we don't unbind it, things end up getting stacked, and tons of ajax things are sent. 
+    $('.meeting_event').click(meeting_event_click)
+}
 
-    // Radio buttons
-    $('#event_AllDay_yes').click(function(){
-	$('#event_AllDay_no').attr('checked',false);
-    });
-    $('#event_AllDay_no').click(function(){
-	$('#event_AllDay_yes').attr('checked',false);
-    });
-    $('#get_ss').click(function(){
-	get_ss();
-    });
+
+/* When one clicks something contained inside a 'meeting_event' we 
+   traverse up the dom looking for the thing that contained the 
+   .meeting_event class. In all cases, it should be a table with an 
+   id. From this ID we are able to get the json object from 'meeting_objs'
+   and from there ask django for more information
+*/
+function meeting_event_click(event){
+    var ME_id = $(event.target).closest('.meeting_event').attr('id');
+    Dajaxice.ietf.meeting.get_info(fill_in_info,{'meeting_obj':meeting_objs[ME_id]} );
+}
+
+
+/* returns a the html for a row in a table 
+   as: <tr><td>title</td><td>data</td></tr>
+*/
+function gen_tr_td(title,data){
+    var trtd = [];
+    
+    trtd.push("<tr>");
+    trtd.push("<td>");
+    trtd.push(title);
+    trtd.push("</td>");
+    trtd.push("<td>");
+    trtd.push(data);
+    trtd.push("</td>");
+    trtd.push("</tr>");
+    
+    return trtd.join(' ');
+}
+
+function generate_info_table(inp){
+    var html = [];
+    html.push("<table>");
+    
+    html.push(gen_tr_td("Group", inp.group));
+    html.push(gen_tr_td("name", inp.name));
+    html.push(gen_tr_td("Requested Time", inp.requested_time));
+    html.push(gen_tr_td("Requested By", inp.requested_by));
+    html.push(gen_tr_td("Time", inp.ts_time));
+    html.push(gen_tr_td("Duration", inp.ts_duration));
+    
+    html.push(gen_tr_td("Room:", inp.room));
     
 
+    html.push("</table>");
+    return html.join(' ');
+
+}
+
+function fill_in_info(inp){
+    $('#ss_info').html(generate_info_table(inp));
 }
 
 function print_all_ss(objs){
     console.log(objs)
-
 }
 function get_ss(){
-    console.log("get_ss")
     Dajaxice.ietf.meeting.get_scheduledsessions(print_all_ss);
 }
 
@@ -144,7 +179,8 @@ function event_template(event_title, description, time, session_id){
     var part1 = "";
     var part2 = "";
     var part3 = "";
-    var part2 = "<table class='meeting_event' id='"+session_id+"'><tr><th>"+event_title+"</th></tr><tr><td style='height:10px'> .."+description+" ..</td></tr></table>"
+    var part2 = "<table class='meeting_event' id='"+session_id+"'><tr id='meeting_event_title'><th>"+event_title+"</th></tr><tr><td> .."+description+" ..</td></tr></table>"
+    // var part2 = "<table class='meeting_event' id='"+session_id+"'><tr id='meeting_event_title'><th>"+event_title+"</th></tr><tr><td style='height:10px'> .."+description+" ..</td></tr></table>"
     return $(part1+part2+part3);
 }
 
@@ -171,8 +207,13 @@ function json_to_id(j){
 }
 
 function id_to_json(id){
-    var split = id.split('_');
-    return {"room":split[0],"date":split[1],"time":split[2]}
+    if(id != null){
+	var split = id.split('_');
+	return {"room":split[0],"date":split[1],"time":split[2]}
+    }
+    else{
+	return null;
+    }
 }
 
 /* following functions are for droppable, they should be reusable */
@@ -186,6 +227,8 @@ function drop_over(event, ui){
 
 function drop_activate(event, ui){
     $(event.draggable).css("background","blue");
+    
+
 }
 
 function drop_out(event, ui){
@@ -195,17 +238,21 @@ function drop_out(event, ui){
 }
 
 function drop_create(event,ui){
-//    console.log("create");
+
+}
+
+function drop_start(event,ui){
 }
 
 function drop_drop(event, ui){
+    
     var temp_id = ui.draggable.attr('id');
     var event_json = id_to_json($(this).attr('id')); // make a json with the new values to inject into the event
     var slot_idd = $(this).attr('id');
     new_event = meeting_objs[temp_id];
     var old_id = json_to_id(new_event);
     var empty = check_free(this);
-
+    
     if(empty){
 	$(this).css("background","");
 	var eTemplate = event_template(new_event.title, 
@@ -213,6 +260,7 @@ function drop_drop(event, ui){
 				       new_event.time, 
 				       new_event.session_id,
 				       new_event.timeslot_id
+				       
 				      );
 	
 	new_event.time = event_json.time;
@@ -220,6 +268,9 @@ function drop_drop(event, ui){
 	new_event.date = event_json.date;
 	
 	$(this).append(eTemplate); // add the html code to the new slot.
+
+
+	console.log($(this));
 
 	ui.draggable.remove(); // remove the old one. 
 	droppable(); // we need to run this again to toggle the new listeners
@@ -241,6 +292,7 @@ function drop_drop(event, ui){
     else{ // happens when you are moving the item to somewhere that has something in it.
 	ui.draggable.css("background",""); // remove the old one. 	
     }
+    listeners();
 }
 
 function drop_bucket(event, ui){
@@ -287,6 +339,7 @@ function drop_bucket(event, ui){
     Dajaxice.ietf.meeting.update_timeslot(dajaxice_callback,{'new_event':new_event});
     
     droppable(); // we need to run this again to toggle the new listeners
+    listeners();
     // console.log(meeting_objs[temp_id]);
     // console.log("done drop_bucket");
     
@@ -294,12 +347,34 @@ function drop_bucket(event, ui){
 
 /* end droppable fucntions */
 
+function drag_drag(event, ui){
+    // console.log("draggiung...");
+    // console.log(this);
+    // $(this).css("background","red");
+    // console.log($(this));
+}
+var ee; 
+function droppable_clone(event){
+    console.log("--------------------------");
+    console.log(event);
+    ee = event
+    var evnt = $(event.target.parentNode.parentNode.parentNode.parentNode);
+    // $(evnt).children();
+    $(evnt).css("width","100px");
+    // console.log(evnt.attr('id'));
+    console.log(evnt.html());
+    return $(evnt) //.html();
+    // return $(event.target.parentNode.parentNode.parentNode.parentNode).html()
+    // console.log("--------------------------");
+}
+
 function droppable(){
     $(function() {
 	/* the thing that is draggable */
 	$( ".meeting_event").draggable({
 	    appendTo: "body",
 	    helper: "clone",
+	    drag: drag_drag,
 	});
 
 	$( "#sortable-list").droppable({
@@ -307,6 +382,7 @@ function droppable(){
 	    activate: drop_activate,
 	    out : drop_out,
 	    drop : drop_bucket,
+	    start: drop_start,
 	})
     
 	$("#meetings td").droppable({
@@ -315,6 +391,7 @@ function droppable(){
 	    out :drop_out,
 	    drop : drop_drop,
 	    create: drop_create,
+	    start: drop_start,
 
 	}); // end $(#meetings td).droppable
     }); // end function()

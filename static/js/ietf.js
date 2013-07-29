909ff534ec1bdc4973903d82ca77727351274059
Lars Eggert <lars@eggert.org>
2013-07-29 11:40:23 +0000

    Use the same code for the top and left nav menu. Minor other tweaks.  - Legacy-Id: 5909

---
+++ static/js/ietf.js
@@ -0,0 +1,8 @@
+/* See http://stackoverflow.com/questions/8878033/how-to-make-twitter-bootstrap-menu-dropdown-on-hover-rather-than-click */
+jQuery('ul.nav li.dropdown').hover(function() {
+	jQuery(this).closest('.dropdown-menu').stop(true, true).show(); 
+	jQuery(this).addClass('open'); }, function() {
+		jQuery(this).closest('.dropdown-menu').stop(true, true).hide(); 
+		jQuery(this).removeClass('open');
+	}
+);

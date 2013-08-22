b43d322af199222f981e0aca8e8e5bc45cd181f0
Lars Eggert <lars@eggert.org>
2013-08-22 08:43:44 +0000

    Update to Bootstrap3. Since it introduced significant changes, the side menu is not quite functional yet.  - Legacy-Id: 6073

--- static/js/ietf.js
+++ static/js/ietf.js
@@ -1,8 +1,8 @@
 /* See http://stackoverflow.com/questions/8878033/how-to-make-twitter-bootstrap-menu-dropdown-on-hover-rather-than-click and http://www.fourfront.us/blog/jquery-window-width-and-media-queries and http://stackoverflow.com/questions/1974788/combine-onload-and-onresize-jquery
 */
 
-$(window).on("debouncedresize", function() {	
-	if ($(".navbar .btn-navbar").css("display") == "none" ) {
+$(window).on("debouncedresize", function() {
+	if ($(".navbar .navbar-toggle").css("display") == "none" ) {
 		jQuery('ul.nav li.dropdown').hover(function() {
 			jQuery(this).closest('.dropdown-menu').stop(true, true).show(); 
 			jQuery(this).addClass('open'); }, function() {

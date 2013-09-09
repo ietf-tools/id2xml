6beec6c3327532487b9bd305047a75c748ca1aa9
Michael Richardson <mcr@sandelman.ca>
2013-09-09 14:45:55 +0000

    setting a colour to empty lets it inherit  - Legacy-Id: 6111

--- v4.70/static/js/timeslot_edit.js
+++ v4.70/static/js/timeslot_edit.js
@@ -32,7 +32,7 @@ var duplicate_sessions = {};
 
 var highlight = "red"; // when we click something and want to highlight it.
 var dragging_color = "blue"; // color when draging events.
-var none_color = '';  // when we reset the color. I believe doing '' will force it back to the stylesheet value.
+var none_color = '';         // unset the color.
 var color_droppable_empty_slot = 'rgb(0, 102, 153)';
 
 // these are used for debugging only.

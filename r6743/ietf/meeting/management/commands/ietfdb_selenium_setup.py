1d0aa568260bf68e03d3a70a97dcde0138c5e153
Michael Richardson <mcr@sandelman.ca>
2013-12-10 02:12:28 +0000

    added command to reload database in appropriate way for selenium/QUnit testing  - Legacy-Id: 6862

---
+++ r6743/ietf/meeting/management/commands/ietfdb_selenium_setup.py
@@ -0,0 +1,30 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from django.core import serializers
+from optparse import make_option
+from django.utils import formats
+import sys
+
+class Command(BaseCommand):
+    help = 'Setups the database for use by selenium and QUnit tests'
+
+    def handle(self, *labels, **options):
+        from django.conf import settings
+        from django.test.utils import get_runner
+
+        from ietf.meeting.helpers import get_meeting,get_schedule
+
+        m83 = get_meeting(83)
+        for schedule in m83.schedule_set.all():
+            schedule.scheduledsession_set.all().delete()
+            schedule.delete()
+
+        # clear a bit more out.
+        m83.session_set.all().delete()
+        m83.room_set.all().delete()
+        m83.timeslot_set.all().delete()
+        m83.delete()
+
+        # (re-)load data from fixtures.
+        call_command('loaddata', 'meeting83')
+

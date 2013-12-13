84c3a7f274bef64193aa31086bbd80b5c6b17d12
Lars Eggert <lars@eggert.org>
2013-12-13 14:56:42 +0000

    Make the submenus work again with Bootstrap 3.  - Legacy-Id: 6913

--- static/js/ietf.js
+++ static/js/ietf.js
@@ -1,17 +1,1 @@
-/* See http://stackoverflow.com/questions/8878033/how-to-make-twitter-bootstrap-menu-dropdown-on-hover-rather-than-click and http://www.fourfront.us/blog/jquery-window-width-and-media-queries and http://stackoverflow.com/questions/1974788/combine-onload-and-onresize-jquery
-*/
-
-$(window).on("debouncedresize", function() {
-	if ($(".navbar .navbar-toggle").css("display") == "none" ) {
-		jQuery('ul.nav li.dropdown').hover(function() {
-			jQuery(this).closest('.dropdown-menu').stop(true, true).show(); 
-			jQuery(this).addClass('open'); }, function() {
-				jQuery(this).closest('.dropdown-menu').stop(true, true).hide(); 
-				jQuery(this).removeClass('open');
-			}
-		);
-	} else {
-		jQuery('ul.nav li.dropdown').off();
-	}
-}).trigger("debouncedresize");
 

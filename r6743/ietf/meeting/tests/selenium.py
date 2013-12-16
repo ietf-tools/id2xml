09399b347a64dde1b34af9bf6fdc4dbdf746e808
Michael Richardson <mcr@sandelman.ca>
2013-12-16 16:33:14 +0000

    added test case 1219: unschedule and schedule a session. Refactored test case controls  - Legacy-Id: 6942

--- r6743/ietf/meeting/tests/selenium.py
+++ r6743/ietf/meeting/tests/selenium.py
@@ -25,15 +25,7 @@ class SeleniumTestCase(django.test.TestCase,RealDatabaseTest):
         self.verificationErrors = []
         self.accept_next_alert = True
 
-    def test_case1218_drag_drop(self):
-        driver = self.driver
-        driver.maximize_window()
-
-        m83 = get_meeting(83)
-        a83 = m83.agenda
-        forces_request = m83.session_set.get(group__acronym = "forces")
-        ipsec_request  = m83.session_set.get(group__acronym = "ipsecme")
-
+    def load_and_wait(self, a83, driver):
         driver.get(self.base_url + "/meeting/83/schedule/%s/edit" % a83.name)
 
         print "The test web server has to run with user wnl who owns mtg83"
@@ -52,16 +44,41 @@ class SeleniumTestCase(django.test.TestCase,RealDatabaseTest):
         self.assertFalse(self.is_element_visible(how=By.CSS_SELECTOR,what="#spinner"))
         print "Found page loaded"
 
-        print "clicking ipsecme"
-        driver.find_element_by_css_selector("#session_%u > tbody > #meeting_event_title > th.meeting_obj" % (ipsec_request.pk)).click()
-        action_chain1 = ActionChains(driver)
-        action_chain2 = ActionChains(driver)
+    def find_forces(self, m83):
+        forces_request = m83.session_set.get(group__acronym = "forces")
 
+        a83 = m83.agenda
         # find current forces timeslot in database
         forces_ss = a83.scheduledsession_set.get(session = forces_request)
         forces_ts = forces_ss.timeslot
         self.assertEqual(forces_ts.location.name, "253", "initial conditions wrong")
         self.assertEqual(forces_ts.time, datetime.datetime(2012,3,26,15,10), "initial time wrong")
+        return forces_request, forces_ss, forces_ts
+
+    def find_forces_rooms(self, m83, driver, forces_ts):
+        monday_room_maillot_ts = m83.timeslot_set.get(location__name = "Maillot",
+                                                  time = datetime.datetime(2012,3,26,15,10))
+
+        monday_room_253  = driver.find_element_by_css_selector("#" + forces_ts.js_identifier)
+        monday_room_maillot = driver.find_element_by_css_selector("#" + monday_room_maillot_ts.js_identifier)
+        return monday_room_253, monday_room_maillot
+
+    def test_case1218_drag_drop(self):
+        driver = self.driver
+        driver.maximize_window()
+
+        m83 = get_meeting(83)
+        a83 = m83.agenda
+        ipsec_request  = m83.session_set.get(group__acronym = "ipsecme")
+
+        self.load_and_wait(a83, driver)
+
+        print "clicking ipsecme"
+        driver.find_element_by_css_selector("#session_%u > tbody > #meeting_event_title > th.meeting_obj" % (ipsec_request.pk)).click()
+        action_chain1 = ActionChains(driver)
+        action_chain2 = ActionChains(driver)
+
+        forces_request, forces_ss,forces_ts = self.find_forces(m83)
 
         print "clicking forces"
         forces = driver.find_element_by_css_selector("#session_%u > tbody > #meeting_event_title > th.meeting_obj" % (forces_request.pk))
@@ -69,12 +86,8 @@ class SeleniumTestCase(django.test.TestCase,RealDatabaseTest):
 
         monday_room_253_ts = m83.timeslot_set.get(location__name = "253",
                                                   time = datetime.datetime(2012,3,26,15,10))
-        monday_room_maillot_ts = m83.timeslot_set.get(location__name = "Maillot",
-                                                  time = datetime.datetime(2012,3,26,15,10))
+        monday_room_253,monday_room_maillot=self.find_forces_rooms(m83, driver, forces_ts)
 
-        monday_room_253  = driver.find_element_by_css_selector("#" + forces_ts.js_identifier)
-        monday_room_maillot = driver.find_element_by_css_selector("#" + monday_room_maillot_ts.js_identifier)
-        print "moving to Maillot slot: %s" % (monday_room_maillot_ts.js_identifier)
         time.sleep(3)
         action_chain1.drag_and_drop(forces, monday_room_maillot).perform()
 
@@ -89,6 +102,49 @@ class SeleniumTestCase(django.test.TestCase,RealDatabaseTest):
 
         print "moving it back to Monday"
         # Have to find it again since we moved it
+        forces = driver.find_element_by_css_selector("#session_%u > tbody > #meeting_event_title > th.meeting_obj" % (forces_request.pk))
+        action_chain2.drag_and_drop(forces, monday_room_253).perform()
+        time.sleep(5)
+
+        forces_ss = a83.scheduledsession_set.get(session = forces_request)
+        forces_ts = forces_ss.timeslot
+        self.assertEqual(forces_ts.location.name, "253")
+        self.assertEqual(forces_ts.time, datetime.datetime(2012,3,26,15,10))
+
+    def test_case1219_unschedule_and_schedule(self):
+        driver = self.driver
+        driver.maximize_window()
+
+        m83 = get_meeting(83)
+        a83 = m83.agenda
+        forces_request = m83.session_set.get(group__acronym = "forces")
+
+        self.load_and_wait(a83, driver)
+
+        action_chain1 = ActionChains(driver)
+
+        forces_request, forces_ss,forces_ts = self.find_forces(m83)
+
+        print "clicking forces"
+        forces = driver.find_element_by_css_selector("#session_%u > tbody > #meeting_event_title > th.meeting_obj" % (forces_request.pk))
+        forces.click()
+
+        unscheduled = driver.find_element_by_css_selector("#sortable-list")
+        action_chain1.drag_and_drop(forces, unscheduled).perform()
+
+        # not sure how else to know it is done yet.
+        print "waiting for database operation to complete"
+        time.sleep(5)
+
+        # assert that it is no longer scheduled
+        self.assertEqual(len(a83.scheduledsession_set.filter(session = forces_request)),0)
+
+        print "moving it back to Monday"
+        action_chain2 = ActionChains(driver)
+
+        monday_room_253,monday_room_maillot=self.find_forces_rooms(m83, driver, forces_ts)
+
+        # Have to find it again since we moved it, and put it back.
         forces = driver.find_element_by_css_selector("#session_%u > tbody > #meeting_event_title > th.meeting_obj" % (forces_request.pk))
         action_chain2.drag_and_drop(forces, monday_room_253).perform()
         time.sleep(5)

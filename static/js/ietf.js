788826a55bd902ae507c26c14bc422eb5a8fdb29
Lars Eggert <lars@eggert.org>
2013-12-19 11:40:36 +0000

    Fix hover actions for bootstrap3.  - Legacy-Id: 7011

--- static/js/ietf.js
+++ static/js/ietf.js
@@ -1,8 +1,19 @@
 // See http://stackoverflow.com/questions/8878033/how-to-make-twitter-bootstrap-menu-dropdown-on-hover-rather-than-click
-jQuery('ul.nav li.dropdown').hover(function() {
-	jQuery(this).closest('.dropdown-menu').stop(true, true).show();
-	jQuery(this).addClass('open');
-}, function() {
-	jQuery(this).closest('.dropdown-menu').stop(true, true).hide();
-	jQuery(this).removeClass('open');
-});
+// Tweaked here, so it only expands on hover for non-collapsed navbars, and works for submenus
+
+function hoverin() {
+	navbar = $(this).closest('.navbar');
+	if (navbar.size() == 0 || navbar.find('.navbar-toggle').is(':hidden')) {
+		$(this).addClass('open');
+	}
+}
+
+function hoverout() {
+	navbar = $(this).closest('.navbar');
+	if (navbar.size() == 0|| navbar.find('.navbar-toggle').is(':hidden')) {
+		$(this).removeClass('open');
+	}
+}
+
+$('ul.nav li.dropdown').hover(hoverin, hoverout);
+$('ul.nav li.dropdown-submenu').hover(hoverin, hoverout);

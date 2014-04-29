018602db25865e4ce662836e7f08ef4ff7fb3756
Lars Eggert <lars@eggert.org>
2014-04-29 11:30:01 +0000

    Various python additions for the facelift UI.  - Legacy-Id: 7668

--- ietf/doc/templatetags/ietf_filters.py
+++ ietf/doc/templatetags/ietf_filters.py
@@ -459,6 +459,24 @@ def format_history_text(text):
     return full
 
 @register.filter
+def format_history_text_facelift(text):
+    """
+    Run history text through some cleaning and add expand button if it's too long.
+    FACELIFT: The original format_history_text can be removed when the facelift
+    templates become default.
+    """
+    full = mark_safe(text)
+
+    if text.startswith("This was part of a ballot set with:"):
+        full = urlize_ietf_docs(full)
+
+    full = mark_safe(keep_spacing(linebreaksbr(urlize_html(sanitize_html(full)))))
+    snippet = truncatewords_html(full, 25)
+    if snippet != full:
+        return mark_safe(u'<div class="snippet">%s<button class="btn btn-xs btn-default show-all pull-right"><span class="caret"></span></button></div><div class="hidden full">%s</div>' % (snippet, full))
+    return full
+
+@register.filter
 def textify(text):
     text = re.sub("</?b>", "*", text)
     text = re.sub("</?i>", "/", text)

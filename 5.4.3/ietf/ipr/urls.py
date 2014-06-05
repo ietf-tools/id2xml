e1366c5c7be1c6b0a8c3c8ae73efa13424c68645
Ryan Cross <rcross@amsl.com>
2014-06-05 23:46:45 +0000

    move dir
    [[Split portion of a mixed commit.]]
     - Legacy-Id: 7863.1

---
+++ 5.4.3/ietf/ipr/urls.py
@@ -0,0 +1,22 @@
+# Copyright The IETF Trust 2007, All Rights Reserved
+
+from django.conf.urls import patterns, url
+from django.views.generic import RedirectView
+from django.core.urlresolvers import reverse_lazy
+
+from ietf.ipr import views, new, search
+
+urlpatterns = patterns('',
+     url(r'^$', views.showlist, name='ipr_showlist'),
+     (r'^about/$', views.about),
+     (r'^by-draft/$', views.iprs_for_drafts_txt),
+     url(r'^(?P<ipr_id>\d+)/old/$', views.show_old, name='ipr_show_old'),
+     url(r'^(?P<id>\d+)/$', views.show, name='ipr_show'),
+     url(r'^(?P<id>\d+)/history/$', views.history, name='ipr_history'),
+     (r'^update/$', RedirectView.as_view(url=reverse_lazy('ipr_showlist'))),
+     (r'^update/(?P<ipr_id>\d+)/$', new.update),
+     (r'^new-(?P<type>specific)/$', new.new),
+     (r'^new-(?P<type>generic)/$', new.new),
+     (r'^new-(?P<type>third-party)/$', new.new),
+     url(r'^search/$', search.search, name="ipr_search"),
+)

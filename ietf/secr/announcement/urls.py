b3e36a1aacffc8fcdcca971726b3b49146b5f08f
Henrik Levkowetz <henrik@levkowetz.com>
2014-06-28 14:54:45 +0000

    - Legacy-Id: 7972

--- ietf/secr/announcement/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('ietf.secr.announcement.views',
-    url(r'^$', 'main', name='announcement'),
-    url(r'^confirm/$', 'confirm', name='announcement_confirm'),
-)
--- ietf/secr/console/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('ietf.secr.console.views',
-    url(r'^$', 'main', name='console'),
-)

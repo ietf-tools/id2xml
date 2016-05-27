1d5d474d48e36a582424815aa9459a6011aa49bc
Ryan Cross <rcross@amsl.com>
2016-05-27 18:47:29 +0000

    add materials file migration  - Legacy-Id: 11250

--- 6.21.0/ietf/meeting/migrations/0024_migrate_interim_meetings.py
+++ 6.21.0/ietf/meeting/migrations/0024_migrate_interim_meetings.py
@@ -2,9 +2,37 @@
 from __future__ import unicode_literals
 
 import datetime
+import os
+import shutil
+import subprocess
 
+from django.conf import settings
 from django.db import migrations
 
+
+def get_old_path(meeting):
+    """Return old path to interim materials file"""
+    path = os.path.join(settings.AGENDA_PATH,
+                        'interim',
+                        meeting.date.strftime('%Y'),
+                        meeting.date.strftime('%m'),
+                        meeting.date.strftime('%d'),
+                        meeting.session_set.first().group.acronym) + '/'
+                        #doc.type_id,
+                        #doc.external_url)
+    return path
+
+def get_new_path(meeting):
+    """Returns new path to document"""
+    return os.path.join(settings.AGENDA_PATH,meeting.number) + '/'
+
+def copy_materials(meeting):
+    """Copy all materials files to new location on disk"""
+    source = get_old_path(meeting)
+    target = get_new_path(meeting)
+    if not os.path.isdir(target):
+        os.makedirs(target)
+    subprocess.call(['rsync','-a',source,target])
 
 def migrate_interim_meetings(apps, schema_editor):
     """For all existing interim meetings create an official schedule and timeslot assignments"""
@@ -35,7 +63,13 @@ def migrate_interim_meetings(apps, schema_editor):
             session=session,
             schedule=meeting.agenda)
 
-
+def migrate_interim_materials_files(apps, schema_editor):
+    """Copy interim materials files to new location"""
+    Meeting = apps.get_model("meeting", "Meeting")
+    
+    for meeting in Meeting.objects.filter(type='interim'):
+        copy_materials(meeting)
+        
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -44,4 +78,5 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunPython(migrate_interim_meetings),
+        migrations.RunPython(migrate_interim_materials_files),
     ]

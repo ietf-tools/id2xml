afa0c8e65ae56568494cb92009c4bca5ea7dcdae
Henrik Levkowetz <henrik@levkowetz.com>
2016-08-09 19:12:15 +0000

    Common development branch for RjS and rcross, for bringing proceedings into the datatracker.
    [[Split portion of a mixed commit.]]
     - Legacy-Id: 11769.4

--- ietf/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright The IETF Trust 2007, All Rights Reserved
-
-__version__ = "5.12.3.dev0"
-
-__date__    = "$Date$"
-
-__rev__     = "$Rev$ (dev) Latest release: Rev. 9206 "
-
-__id__      = "$Id$"
--- ietf/release/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns
-
-urlpatterns = patterns('',
-    (r'^$',  'ietf.release.views.release'),
-    (r'^(?P<version>[0-9.]+.*)/$',  'ietf.release.views.release'),
-)
-

75790879c97ed0e4ce50edd0b39eabeb1c61b220
Henrik Levkowetz <henrik@levkowetz.com>
2016-08-09 19:34:01 +0000

    Rearranging branches some more ...
    [[Split portion of a mixed commit.]]
     - Legacy-Id: 11774.2

--- branch/proceedings_tmp/ietf/dbtemplate/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-
-
-urlpatterns = patterns('ietf.dbtemplate.views',
-    url(r'^(?P<acronym>[-a-z0-9]+)/$', 'template_list', name='template_list'),
-    url(r'^(?P<acronym>[-a-z0-9]+)/(?P<template_id>[\d]+)/$', 'template_edit', name='template_edit'),
-)

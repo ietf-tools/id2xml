024e6d53781fe9b3eb5b6b26f6806d0cf79c09ab
Henrik Levkowetz <henrik@levkowetz.com>
2016-08-09 19:12:15 +0000

    Common development branch for RjS and rcross, for bringing proceedings into the datatracker.
    [[Split portion of a mixed commit.]]
     - Legacy-Id: 11769.9

--- ietf/doc/urls_material.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('ietf.doc.views_material',
-    url(r'^(?P<action>state|title|abstract|revise)/$', "edit_material", name="material_edit"),
-)
-

854729568494e3c7dc761790057b509cd062c8ba
Henrik Levkowetz <henrik@levkowetz.com>
2016-08-09 19:12:15 +0000

    Common development branch for RjS and rcross, for bringing proceedings into the datatracker.
    [[Split portion of a mixed commit.]]
     - Legacy-Id: 11769.6

--- ietf/person/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns
-from ietf.person import ajax
-
-urlpatterns = patterns('',
-        (r'^search/(?P<model_name>(person|email))/$', "ietf.person.views.ajax_select2_search", None, 'ajax_select2_search_person_email'),
-        (r'^(?P<personid>[a-z0-9]+).json$', ajax.person_json),
-)

9b66649860df81d283598ea60408c24720094e4a
Seth Birkholz <sbirkholz@amsl.com>
2017-06-06 15:05:25 +0000

    - improved form validation - closing datepicker after selection  - Legacy-Id: 13542

--- ietf/sidemeeting/forms.py
+++ ietf/sidemeeting/forms.py
@@ -1,4 +1,4 @@
-
+import re
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 from collections import OrderedDict
@@ -42,7 +42,63 @@ class SideMeetingForm(forms.ModelForm):
         super(SideMeetingForm, self).__init__(*args, **kwargs)
         # there's a `fields` property now
         self.fields['group'].required = False
-        self.fields['resources'].required = False        
+        self.fields['resources'].required = False
+
+    def clean_name(self):
+        '''Date field validator.  We can't use required on the input because
+        it is a datepicker widget'''
+        name = self.cleaned_data.get('name')
+        if not name:
+            raise forms.ValidationError('Required field')
+        return name
+        
+    def clean_attendees(self):
+        '''clean '''
+        name = "attendees"
+        attendees = self.cleaned_data.get(name)
+        if not name:
+            raise forms.ValidationError('Required field')
+        count = None
+        try:
+            count = int(attendees)
+        except:
+            raise forms.ValidationError('Invalid attendance' )
+        if (count < 1) or (count > 1000000):
+            raise forms.ValidationError('Invalid number of %s' % name  )
+        
+        return attendees
+        
+    def clean_requested_start_time(self):
+        '''Date field validator.  We can't use required on the input because
+        it is a datepicker widget'''
+        name = 'requested_start_time'
+        
+        requested_start_time = re.sub('\s+','',self.cleaned_data.get(name))
+        if not name:
+            raise forms.ValidationError('%s is required' % name )
+        pieces = requested_start_time.split(":")
+        hour = None
+        minutes = None
+        
+        try:
+            hour = int(pieces[0])
+        except:
+           raise forms.ValidationError('Invalid start time')            
+        if hour < 0 or hour > 24:
+           raise forms.ValidationError('Invalid start time')
+       
+        if len(pieces) > 1:
+            try:
+               minutes = int(pieces[1])
+            except:
+               raise forms.ValidationError('Invalid start time')
+            if minutes < 0 or minutes > 59:
+               raise forms.ValidationError('Invalid start time')                                 
+        if not minutes:
+            minutes = 0
+        requested_start_time = "%02d:%02d" % (hour, minutes)
+        return requested_start_time
+        
         
     class Meta:
         model = models.SideMeetingSession

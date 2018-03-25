753a680cc17f0fe04abd770f1ca488386737fcd0
Henrik Levkowetz <henrik@levkowetz.com>
2018-03-25 18:20:41 +0000

    Added a link to meeting attendance stats to the top right corner of the meeting header.  - Legacy-Id: 14928

--- ietf/meeting/models.py
+++ ietf/meeting/models.py
@@ -171,6 +171,12 @@ class Meeting(models.Model):
     def get_schedule_by_name(self, name):
         return self.schedule_set.filter(name=name).first()
 
+    def get_number(self):
+        if self.number.isdigit():
+            return int(self.number)
+        else:
+            return None
+
     @property
     def sessions_that_can_meet(self):
         qs = self.session_set.exclude(status__slug='notmeet').exclude(status__slug='disappr').exclude(status__slug='deleted').exclude(status__slug='apprw')

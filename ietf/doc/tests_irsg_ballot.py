2f4307e063c07f08eaf712fdec32f141de6d1600
Peter E. Yee <github@houseofyee.com>
2019-09-13 23:07:59 +0000

    State as of September 13  - Legacy-Id: 16733

--- ietf/doc/tests_irsg_ballot.py
+++ ietf/doc/tests_irsg_ballot.py
@@ -9,10 +9,10 @@ import debug                            # pyflakes:ignore
 
 from django.urls import reverse as urlreverse
 
-#from ietf.utils.test_utils import TestCase, login_testing_unauthorized, unicontent
-from ietf.utils.test_utils import TestCase, unicontent
+from ietf.utils.test_utils import TestCase, login_testing_unauthorized, unicontent
 from ietf.doc.factories import IndividualDraftFactory, WgDraftFactory, RgDraftFactory
 from ietf.doc.models import BallotDocEvent
+from ietf.person.utils import get_active_irsg, get_active_ads
 
 class IssueIRSGBallotTests(TestCase):
 
@@ -54,6 +54,9 @@ class IssueIRSGBallotTests(TestCase):
         self.assertEqual(r.status_code,200)
         self.assertNotIn("Issue IRSG ballot", unicontent(r))
 
+        # PEY Is this needed?  Or will the client be logged out between functions?
+        self.client.logout()
+
     def test_issue_ballot(self):
 
         # Just testing IRTF drafts
@@ -90,3 +93,33 @@ class IssueIRSGBallotTests(TestCase):
         r = self.client.get(url)
         self.assertEqual(r.status_code,200)
         self.assertNotIn("Issue IRSG ballot", unicontent(r))
+
+        # The IRSG evaluation record tab should exist
+        self.assertIn("IRSG evaluation record", unicontent(r))
+        # The IRSG evaluation record tab should not indicate unavailability
+        self.assertNotIn("IRSG Evaluation Ballot has not been created yet", unicontent(r))
+
+        # We should find an IRSG member's name on the IRSG evaluation tab regardless of any positions taken or not
+        url = urlreverse('ietf.doc.views_doc.document_irsg_ballot',kwargs=dict(name=rg_draft.name))
+        r = self.client.get(url)
+        self.assertEqual(r.status_code,200)
+        irsgmembers = get_active_irsg()
+        self.assertNotEqual(len(irsgmembers), 0)
+        self.assertIn(irsgmembers[0].name, unicontent(r))
+
+        # Also issue a contemporaneous IESG ballot
+        url = urlreverse('ietf.doc.views_ballot.ballot_writeupnotes', kwargs=dict(name=rg_draft.name))
+        login_testing_unauthorized(self, "ad", url)
+        
+        r = self.client.post(url, dict(
+                ballot_writeup="This is a test.",
+                issue_ballot="1"))
+        self.assertEqual(r.status_code, 200)
+
+        # Look for IESG member name in the ballot list
+        url = urlreverse('ietf.doc.views_doc.document_ballot',kwargs=dict(name=rg_draft.name))
+        r = self.client.get(url)
+        self.assertEqual(r.status_code,200)
+        iesgmembers = get_active_ads()
+        self.assertNotEqual(len(iesgmembers), 0)
+        self.assertIn(iesgmembers[0].name, unicontent(r))
